13dd0907609049209f7ad9af3ed3ec23c17e7b3e
owermaiden <ower.maiden@cybertekschool.com>
2023-08-02 15:14:37 -0400

    xcomm

--- src/app/xcomm/child/child.component.ts
+++ src/app/xcomm/child/child.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent {
-
+  selectedMentor: string = 'Ower';
 }
--- src/app/xcomm/parent/parent.component.ts
+++ src/app/xcomm/parent/parent.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
-export class ParentComponent {
+export class ParentComponent implements OnInit{
+  mentors: string[] = ['Ower', 'Matt', 'Sahin'];
+  selectedMentor: string = 'Ower';
+  constructor(){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onSelectMentor(mentor: string){
+    this.selectedMentor = mentor;
+    console.log(this.selectedMentor);
+  }
+
 
 }
