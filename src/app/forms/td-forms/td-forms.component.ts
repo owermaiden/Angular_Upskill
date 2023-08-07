caef8599f8cee6a54d449c8ed5550f0a987d5762
owermaiden <ower.maiden@cybertekschool.com>
2023-08-07 14:39:18 -0400

    tdforms

--- src/app/forms/td-forms/td-forms.component.ts
+++ src/app/forms/td-forms/td-forms.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-td-forms',
   templateUrl: './td-forms.component.html',
   styleUrls: ['./td-forms.component.css']
 })
-export class TdFormsComponent {
+export class TdFormsComponent implements OnInit{
+
+  constructor(){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onSubmit(form: any){
+    console.log(form.value);
+  }
+
 
 }
