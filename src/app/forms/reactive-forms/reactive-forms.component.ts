4e43539ebfad5d790de1173158d99888de6dbe08
owermaiden <ower.maiden@cybertekschool.com>
2023-08-07 15:01:22 -0400

    latest

--- src/app/forms/reactive-forms/reactive-forms.component.ts
+++ src/app/forms/reactive-forms/reactive-forms.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   templateUrl: './reactive-forms.component.html',
   styleUrls: ['./reactive-forms.component.css']
 })
-export class ReactiveFormsComponent {
+export class ReactiveFormsComponent implements OnInit {
+  myFrom: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(){
+    this.myFrom = this.fb.group({
+      name: '',
+      email: '',
+      message: ''
+    });
+  }
+
+  onSubmit(){
+    console.log(this.myFrom);
+  }
+
+
+
 
 }
